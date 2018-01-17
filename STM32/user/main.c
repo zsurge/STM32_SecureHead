e6519be31deb50bffa416a099a5fba13d49234e3
surge <zdsurge@163.com>
2018-01-17 09:32:15 +0800

    APP 改为使用外部晶振

--- STM32/user/main.c
+++ STM32/user/main.c
@@ -31,7 +31,7 @@
 #define PinOUT2 GPIO_Pin_0
 #define PinOUT3 GPIO_Pin_1
 
-
+extern ErrorStatus HSEStartUpStatus;
 void RCC_Configuration(void);
 void GPIO_Configuration(void);
 void delay_ms(uint16_t nms);
@@ -46,50 +46,103 @@ void delay_us(uint32_t Nus);
 * Date           : 2017-03-05
 *******************************************************************************/
 void RCC_Configuration(void)
+//{
+//	//将外设 RCC寄存器重设为缺省值
+//	RCC_DeInit();
+
+//	//使能内部晶振，内部晶振频率8MHz
+//	RCC_HSICmd(ENABLE);
+//  
+//	//等待HSI晶振就绪
+//	while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
+//	
+//	//开启FLASH预取指功能
+//	FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+//	
+//	//FLASH时序控制
+//	FLASH_SetLatency(FLASH_Latency_2);
+//   
+//	//设置HCLK（AHB时钟）=SYSCLK
+//	RCC_HCLKConfig(RCC_SYSCLK_Div1);
+//   
+//	//PCLK2(APB2) = HCLK
+//	RCC_PCLK2Config(RCC_HCLK_Div1);
+//  
+//	//PCLK1(APB1) = HCLK/2
+//	RCC_PCLK1Config(RCC_HCLK_Div2);
+//    
+//	//设置PLL时钟源及倍频系数:8M * 9 = 72MHz
+//	RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_9);   
+//                
+//	//使能PLL
+//	RCC_PLLCmd(ENABLE);
+
+//	//等待指定的 RCC 标志位设置成功 等待PLL初始化成功
+//	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);	
+
+//	//设置系统时钟（SYSCLK） 设置PLL为系统时钟源
+//	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+//  
+//	//等待PLL成功用作于系统时钟的时钟源
+//	//  0x00：HSI 作为系统时钟 
+//	//  0x04：HSE作为系统时钟 
+//	//  0x08：PLL作为系统时钟  
+//	while(RCC_GetSYSCLKSource() != 0x08);	
+//}
 {
-	//将外设 RCC寄存器重设为缺省值
-	RCC_DeInit();
 
-	//使能内部晶振，内部晶振频率8MHz
-	RCC_HSICmd(ENABLE);
-  
-	//等待HSI晶振就绪
-	while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
-	
-	//开启FLASH预取指功能
-	FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-	
-	//FLASH时序控制
-	FLASH_SetLatency(FLASH_Latency_2);
-   
-	//设置HCLK（AHB时钟）=SYSCLK
-	RCC_HCLKConfig(RCC_SYSCLK_Div1);
-   
-	//PCLK2(APB2) = HCLK
-	RCC_PCLK2Config(RCC_HCLK_Div1);
-  
-	//PCLK1(APB1) = HCLK/2
-	RCC_PCLK1Config(RCC_HCLK_Div2);
+  /*将外设RCC寄存器重设为缺省值*/
+  RCC_DeInit(); 
+
+  /*设置外部高速晶振（HSE）*/
+  RCC_HSEConfig(RCC_HSE_ON);   //RCC_HSE_ON——HSE晶振打开(ON) 
+
+  /*等待HSE起振*/
+  HSEStartUpStatus = RCC_WaitForHSEStartUp(); 
+
+  if(HSEStartUpStatus == SUCCESS)        //SUCCESS：HSE晶振稳定且就绪
+  {
+    /*设置AHB时钟（HCLK）*/ 
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);  //RCC_SYSCLK_Div1——AHB时钟= 系统时钟 
+
+    /* 设置高速AHB时钟（PCLK2）*/ 
+    RCC_PCLK2Config(RCC_HCLK_Div1);   //RCC_HCLK_Div1——APB2时钟= HCLK 
+
+    /*设置低速AHB时钟（PCLK1）*/    
+    RCC_PCLK1Config(RCC_HCLK_Div2);   //RCC_HCLK_Div2——APB1时钟= HCLK / 2 
+
+    /*设置FLASH存储器延时时钟周期数*/
+    FLASH_SetLatency(FLASH_Latency_2);    //FLASH_Latency_2  2延时周期   
+
+    /*选择FLASH预取指缓存的模式*/  
+    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);       // 预取指缓存使能 
+
+    /*设置PLL时钟源及倍频系数*/ 
+    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);     // PLL的输入时钟= HSE时钟频率；RCC_PLLMul_9——PLL输入时钟x 9   
     
-	//设置PLL时钟源及倍频系数:8M * 9 = 72MHz
-	RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_9);   
-                
-	//使能PLL
-	RCC_PLLCmd(ENABLE);
+    /*使能PLL */
+    RCC_PLLCmd(ENABLE);  
 
-	//等待指定的 RCC 标志位设置成功 等待PLL初始化成功
-	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);	
+    /*检查指定的RCC标志位(PLL准备好标志)设置与否*/   
+    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)      
+   {
+    ;
+   } 
 
-	//设置系统时钟（SYSCLK） 设置PLL为系统时钟源
-	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-  
-	//等待PLL成功用作于系统时钟的时钟源
-	//  0x00：HSI 作为系统时钟 
-	//  0x04：HSE作为系统时钟 
-	//  0x08：PLL作为系统时钟  
-	while(RCC_GetSYSCLKSource() != 0x08);	
+    /*设置系统时钟（SYSCLK）*/ 
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); //RCC_SYSCLKSource_PLLCLK——选择PLL作为系统时钟 
+
+    /* PLL返回用作系统时钟的时钟源*/
+    while(RCC_GetSYSCLKSource() != 0x08)        //0x08：PLL作为系统时钟
+   { 
+        ;
+   }
+
+ } 
+
+ /*使能或者失能APB2外设时钟*/
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO , ENABLE); 
 }
-
 /*******************************************************************************
 * Function Name  : NVIC_Configuration
 * Description    : NVIC设置
@@ -183,7 +236,7 @@ void EXTI_Configuration(void)
 	NVIC_InitTypeDef NVIC_InitStructure;
 	EXTI_InitTypeDef EXTI_InitStructure;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB|RCC_APB2Periph_AFIO, ENABLE);
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB|RCC_APB2Periph_AFIO, ENABLE);
 	
 	//配置外部中断事件
 	/*外部中断使用PB2*/												//初始化 PB2		CPD
@@ -426,18 +479,17 @@ int main(void)
 	
 	delay_init(72);
 
+	//打开串口
+	dev_com_open();		
+	uart_puts("APP Start...\r\n");
+
 	#endif
 
 	//USB INIT
 	UsbAllInit();	
 	UsbRecvReset();
 
-
-#if 1
-	//打开串口
-	dev_com_open();		
-	uart_puts("APP Start...\r\n");
-	
+#if 1	
 	
 	//打开定时器计时
 	TIM2_Cap_Init(72 - 1);										//以 1Mhz 的频率计数
